269f8df05bb744b9441ab78c33de00d2ebe8346a
hsbx520 <79846141+hsbx520@users.noreply.github.com>
2025-11-22 07:31:56 +0800

    Update COUNTDOWN_TARGET_UTC to new date

--- script.js
+++ script.js
@@ -465,7 +465,7 @@ const PRESALE_CONFIG = {
         USDT: "Ethereum or BNB Smart Chain.",
         USDC: "Ethereum or BNB Smart Chain."
     },
-    COUNTDOWN_TARGET_UTC: "2025-11-21T19:00:00Z"
+    COUNTDOWN_TARGET_UTC: "2025-11-22T05:00:00Z"
 };
 
 // Presale State
